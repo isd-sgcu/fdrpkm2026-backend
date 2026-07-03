68a4087257f2cf78cb5cad09966a84d78298f56e
Patsagorn Yuenyong <works@ptsgrn.dev>
2026-07-04 03:24:08 +0700

    feat: enhance authentication setup with Google provider and email validation

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,9 +1,29 @@
 import { betterAuth } from "better-auth/minimal";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@src/db";
+import { env } from "@src/config";
+import { APIError, createAuthMiddleware } from "better-auth/api";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg"
-  })
+  }),
+  appName: "fdrpkm2026",
+  socialProviders: {
+    google: {
+      clientId: env.GOOGLE_CLIENT_ID || "",
+      clientSecret: env.GOOGLE_CLIENT_SECRET || ""
+    }
+  },
+  secret: env.BETTER_AUTH_SECRET || "",
+  baseURL: env.BETTER_AUTH_URL || "",
+  hooks: {
+    before: createAuthMiddleware(async (ctx) => {
+      if (!ctx.body?.email.endsWith("@student.chula.ac.th")) {
+        throw new APIError("BAD_REQUEST", {
+          message: "Email must end with @student.chula.ac.th"
+        });
+      }
+    })
+  }
 });
